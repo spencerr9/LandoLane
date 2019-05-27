1afdc761a1ed620c216e6df8bd1eea28133366a1
Spencer Ricks <spencerricks@Spencers-MacBook-Pro.local>
2019-05-27 00:16:14 -0600

    Worked on mobile-responsiveness on navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,35 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import '../../reset.css'
-import './NavBar.css'
+import "../../reset.css";
+import "./NavBar.css";
 
-export default function NavBar(){
-    return (
-        <div 
-            id="wrapper"
-            // style={{
-            //     display: "flex",
-            //     alignItems: "center",
-            //     justifyContent: "space-evenly",
-            //     textTransform: "uppercase",
-            //     height: "200px",
-            //     backgroundColor: "skyblue"
-            // }}
-        >
-            <Link to='/'>Home</Link>
-            <Link to='/weddings'>Weddings</Link>
-            <Link to='/parties'>Parties</Link>
-            <img 
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Pan_Am_Logo.svg/1200px-Pan_Am_Logo.svg.png" 
-                alt=""
-                // style={{
-                //     height: "100px",
-                // }}
-                ></img>
-            <Link to='/events'>Events</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/contact'>Contact</Link>
-        </div>
-    )
+import NavDrawer from "./NavDrawer";
+
+export default function NavBar() {
+  return (
+    <div id="wrapper">
+      <div id="navLinks">
+        <Link to="/">Home</Link>
+        <Link to="/weddings">Weddings</Link>
+        <Link to="/parties">Parties</Link>
+        <Link to="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Pan_Am_Logo.svg/1200px-Pan_Am_Logo.svg.png"
+            alt=""
+          />
+        </Link>
+        <Link to="/events">Events</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+      <div id="drawerWrapper">
+        <Link to="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Pan_Am_Logo.svg/1200px-Pan_Am_Logo.svg.png"
+            alt=""
+          />
+        </Link>
+        <NavDrawer />
+      </div>
+    </div>
+  );
 }
